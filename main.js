053bd92e091a906d4a6dfff8f60d1e3b3428d05e
francienevaz <fran.vfraga@gmail.com>
2021-12-31 09:13:35 -0300

    criando um contador

---
+++ main.js
@@ -0,0 +1,27 @@
+let count = 0;
+
+let currentNumberWrapper = document.getElementById('currentNumber');
+
+function increment() {
+    count++;
+    currentNumberWrapper.innerHTML = count;
+}
+
+function decrement() {
+    count--;
+    currentNumberWrapper.innerHTML = count;
+}
+
+
+/* var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment () {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement () {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}*/

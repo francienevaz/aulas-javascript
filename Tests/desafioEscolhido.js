19acfb197e80a645693274e08a628b3e9395851e
francienevaz <fran.vfraga@gmail.com>
2022-04-27 15:18:56 -0300

    updated

--- Tests/desafioEscolhido.js
+++ Tests/desafioEscolhido.js
@@ -1,30 +1,62 @@
 //TODO: Complete os espaços em branco com uma solução possível para o problema.
-let line = gets();
-let matricula;
+// let line = gets();
+// let matricula;
 
-for(let i = 0; i < line; i++ ){
-  line = gets();
-  line = line.split(" ")
-  if(parseFloat(line[1]) >= 8){
-    matricula = parseInt(line[0]);
-    print(matricula);
-  } else {
-  print("Minimum note not reached")
-}
-}
+// for(let i = 0; i < line; i++ ){
+//   line = gets();
+//   line = line.split(" ")
+//   if(parseFloat(line[1]) >= 8){
+//     matricula = parseInt(line[0]);
+//     print(matricula);
+//   } else {
+//   print("Minimum note not reached")
+// }
+// }
 
+// const gets = (() => {
+//   let array = [ 3, 
+//     1000, 5,
+//     1001, 10,
+//     1002, 6];
+//   return array;
+// });
+
+// let nAlunos = parseInt(gets());
+// let nMatricula;
+// let nota;
+
+// for(let i = 0; i <= nAlunos; i++) {
+//     nAlunos = gets();
+//     nAlunos = nAlunos.split(" ");
+//     nota = parseFloat(nAlunos[1]);
+//     if( nota < 8) {
+//         console.log("Minimum note not reached")
+//     } else if (nota >= 8) {
+//         nMatricula = parseInt(nAlunos[0]);
+//         console.log(nMatricula);
+//     }
+// }
+
+// Solução
+//TODO: Complete os espaços em branco com uma solução possível para o problema.
 let nAlunos = parseInt(gets());
-let nMatricula;
-let nota;
+let nMatricula = Array(nAlunos);
+let nota = Array(nAlunos);
 
-for(let i = 0; i <= nAlunos; i++) {
-    nAlunos = gets();
-    nAlunos = nAlunos.split(" ");
-    nota[i] = parseFloat(nAlunos[1]);
-    if( nota < 8) {
-        console.log("Minimum note not reached")
-    } else if (nota >= 8) {
-        nMatricula[i] = parseInt(nAlunos[0]);
-        console.log(nMatricula);
-    }
+for (i = 0; i < nAlunos; i++) {
+let aluno = gets().split(' ');
+ nMatricula[i] = parseInt(aluno[0]);
+ nota[i] = parseFloat(aluno[1]);
 }
+let notaMax = nota.reduce( (a, b) => {
+ return Math.max(a, b);
+});
+for (i = 0; i < nAlunos; i++) {
+ if (notaMax < 8) {
+console.log('Minimum note not reached');
+ break;
+ } else if (nota[i] === notaMax) {
+console.log(nMatricula[i]);
+ }
+}
+

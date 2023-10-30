1dab31f127032a97a22fc025c0eaffb7943d2d62
francienevaz <fran.vfraga@gmail.com>
2023-10-30 18:20:11 -0300

    updated

---
+++ JavaScript-Essencial/Operadores/atribuicao.js
@@ -0,0 +1,22 @@
+// Atribuição
+x = y
+
+//Atribuição adição
+x = x + y // ou
+x += y
+
+//Atribuição subtração
+x = x - y // ou
+x -= y
+
+//Atribuição multiplicação
+x = x * y // ou
+x *= y
+
+//Atribuição divisão
+x = x / y // ou
+x /= y
+
+//Atribuição de resto
+x = x % y // ou
+x %= y
---
+++ JavaScript-Essencial/Operadores/spread.js
@@ -0,0 +1,9 @@
+let cesta = ['banana', 'uva', 'maçã'];
+
+let cestaFrutas = ['abacate', ...cesta, 'abacaxi', 'morango']
+
+
+function fn(a, b, c){};
+let args = [ 0 , 1 , 2]
+fn(...args);
+// o spread operator simplifica a leitura e a codificação 

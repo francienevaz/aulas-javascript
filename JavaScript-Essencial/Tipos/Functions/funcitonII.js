f2b6a12bdb9dfb454f91ce91ea1e0c0aa4698856
francienevaz <fran.vfraga@gmail.com>
2023-10-30 17:08:05 -0300

    updated

--- JavaScript-Essencial/Tipos/Functions/funcitonII.js
+++ JavaScript-Essencial/Tipos/Functions/funcitonII.js
@@ -20,3 +20,25 @@
     console.log(user.getNameArrowFn());
     console.log(user.getName())
 })()
+
+(()=> {
+    this.name = 'José'
+
+    function fnTradicional() {
+        return `Hello ${this.name}!`;
+    }
+
+    const obj = {
+        name: 'Maria',
+        greet: () => {
+          console.log(`Olá, ${this.name}`);
+        },
+        fnTradicional
+      };
+    
+    console.log(obj.fnTradicional()) // retorna Hello Maria!, pois o this referencia o objeto, por ser uma function tradicional
+    obj.greet(); // "Olá, José" (o valor de this não se refere ao objeto "obj")
+      
+})()
+
+//Arrow functions o this é léxico, ou seja, é fixo, consistente e previsível, dependende do contexto, em functions tradicionais o this é mais flexível podendo referênciar outros valores dependendo do contexto onde está inserido.
